ad9d0b675f855c3f3aa27696d4eda6aa685e6214
Beyza Atalay <beyatala@k1m07s01.42istanbul.com.tr>
2026-09-02 20:38:21 +0300

    Fix ft_strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: beyatala <beyatala@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/05 13:02:36 by beyatala          #+#    #+#             */
-/*   Updated: 2026/08/24 15:16:03 by beyatala         ###   ########.fr       */
+/*   Updated: 2026/09/02 19:37:19 by beyatala         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,9 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	j;
 	size_t	k;
 
-	i = 0;
-	j = 0;
-	while (i < size && dst[i])
-		i++;
-	while (src[j])
-		j++;
-	if (i == size)
+	i = ft_strlen(dst);
+	j = ft_strlen(src);
+	if (i <= size)
 		return (size + j);
 	k = 0;
 	while (i + k < size - 1 && src[k])
